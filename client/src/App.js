3e2aa56168a8e35c75d899d1dc4e39e23da08083
Tim Ninan <tim.ninan@gmail.com>
2018-02-24 14:58:16 -0600

    tested with axios call to make sure proxy works

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  onClick = () => {
+    console.log('I was clicked');
+    axios.get('/api')
+      .then(function(response){
+        console.log(response);
+      }); 
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.onClick}>Click Me</button>
       </div>
     );
   }
