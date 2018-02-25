17c0e9519e59701c1307f68ab0cc4d97f4805f9f
Tim Ninan <tim.ninan@gmail.com>
2018-02-25 15:09:01 -0600

    added basic homepage semantic ui layout

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Homepage from './Homepage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Homepage />, document.getElementById('root'));
