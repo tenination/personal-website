532b99c403fb6f8767a352c608d23cf5f2a83882
Tim Ninan <tim.ninan@gmail.com>
2018-02-26 01:46:25 -0600

    added copyright section

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -290,6 +290,8 @@ const Homepage = () => (
           <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
             <Header as='h3' style={{ fontSize: '2em' }}>My Goal Posts</Header>
              <p style={{fontStyle:'italic', fontSize:'30px'}}>Keep track of your goals online</p>
+             <br />
+             <br />
             <img src={MyGoalPosts} style={{width:'488px', height:'226px'}}></img>
             <br/>
             <img src={angularLogo} style={{height:'100px', width:'100px', padding: '15px'}}></img>
@@ -305,62 +307,17 @@ const Homepage = () => (
       </Grid>
     </Segment>
 
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Container text>
-        <Header as='h3' style={{ fontSize: '2em' }}>Breaking The Grid, Grabs Your Attention</Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Instead of focusing on content creation and hard work, we have learned how to master the art of doing
-          nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic
-          and worth your attention.
-        </p>
-        <Button as='a' size='large'>Read More</Button>
-        <Divider
-          as='h4'
-          className='header'
-          horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-        >
-          <a href='#'>Case Studies</a>
-        </Divider>
-        <Header as='h3' style={{ fontSize: '2em' }}>Did We Tell You About Our Bananas?</Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but it's really
-          true.
-          It took years of gene splicing and combinatory DNA research, but our bananas can really dance.
-        </p>
-        <Button as='a' size='large'>I'm Still Quite Interested</Button>
-      </Container>
-    </Segment>
     <Segment inverted vertical style={{ padding: '5em 0em' }}>
       <Container>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='About' />
-              <List link inverted>
-                <List.Item as='a'>Sitemap</List.Item>
-                <List.Item as='a'>Contact Us</List.Item>
-                <List.Item as='a'>Religious Ceremonies</List.Item>
-                <List.Item as='a'>Gazebo Plans</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as='h4' inverted>Footer Header</Header>
-              <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+          <h1 style={{textAlign:'center', fontSize:'20px'}}>&copy; 2018 Tim Ninan</h1>
+          <div style={{textAlign:'center'}}>
+          <span style={{fontSize:'15px', marginLeft:'25px'}}>Created using React.js and lots of</span>
+          <span style={{color:'black'}}> ...</span>
+          <Icon name='heart' size='large' color='red' />
+          </div>
       </Container>
     </Segment>
+
   </ResponsiveContainer>
 )
 export default Homepage
