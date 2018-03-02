258bd194d689d6103aa384d59a23482ea30a571d
Tim Ninan <tim.ninan@gmail.com>
2018-03-02 13:13:08 -0600

    added tablet responsiveness

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types'
 import {
   Button,
   Container,
@@ -21,7 +20,7 @@ import 'semantic-ui-css/semantic.min.css';
 import keyboard from './keyboard.jpg'
 // frontend
 import reactLogo from './reactLogo.png';
-import reduxLogo from './reduxLogo.png';
+import reduxLogo  from './reduxLogo.png';
 import angularLogo from './angularLogo.png';
 import backboneLogo from './backboneLogo.png';
 import jqueryLogo from './jqueryLogo.png';
@@ -81,101 +80,57 @@ const HomepageHeading = ({ mobile }) => (
   </Container>
 )
 
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-}
-
-/* Heads up!
- * Neither Semantic UI, nor Semantic UI React don't offer a responsive navbar, hover it can be easily implemented.
- * It can be more complicated, but you can create really flexible markup.
- */
 class DesktopContainer extends Component {
-  state = {}
-
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
 
   render() {
     const { children } = this.props
-    const { fixed } = this.state
-
     return (
       <Responsive {...Responsive.onlyComputer}>
-        <Visibility once={false} onBottomPassed={this.showFixedMenu} onBottomPassedReverse={this.hideFixedMenu}>
           <Segment inverted textAlign='center' style={{ minHeight: 700, padding: '1em 0em', backgroundImage:`url(${keyboard})`, backgroundSize:'cover'  }} vertical>
             <HomepageHeading />
           </Segment>
-        </Visibility>
-
         {children}
       </Responsive>
     )
   }
 }
 
-DesktopContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-class MobileContainer extends Component {
-  state = {}
-
-  handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened })
-
+class TabletContainer extends Component {
   render() {
     const { children } = this.props
-    const { sidebarOpened } = this.state
-
     return (
       <Responsive {...Responsive.onlyMobile}>
-        <Sidebar.Pushable>
-          <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
-            <Menu.Item as='a' active>Home</Menu.Item>
-            <Menu.Item as='a'>Work</Menu.Item>
-            <Menu.Item as='a'>Company</Menu.Item>
-            <Menu.Item as='a'>Careers</Menu.Item>
-            <Menu.Item as='a'>Log in</Menu.Item>
-            <Menu.Item as='a' primary>Sign Up</Menu.Item>
-          </Sidebar>
-
-          <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handleToggle} style={{ minHeight: '100vh' }}>
-            <Segment inverted textAlign='center' style={{ minHeight: 350, padding: '1em 0em' }} vertical>
-              <Container>
-                <Menu inverted pointing secondary size='large'>
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name='sidebar' />
-                  </Menu.Item>
-                  <Menu.Item position='right'>
-                    <Button as='a' inverted>Log in</Button>
-                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
-                  </Menu.Item>
-                </Menu>
-              </Container>
+            <Segment inverted textAlign='center' style={{ minHeight: 350, padding: '1em 0em', backgroundImage:`url(${keyboard})`, backgroundSize:'cover'  }} vertical>
               <HomepageHeading mobile />
             </Segment>
-
             {children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
       </Responsive>
     )
   }
 }
 
-MobileContainer.propTypes = {
-  children: PropTypes.node,
+
+class MobileContainer extends Component {
+  render() {
+    const { children } = this.props
+    return (
+      <Responsive {...Responsive.onlyTablet}>
+            <Segment inverted textAlign='center' style={{ minHeight: 350, padding: '1em 0em', backgroundImage:`url(${keyboard})`, backgroundSize:'cover'  }} vertical>
+              <HomepageHeading mobile />
+            </Segment>
+            {children}
+      </Responsive>
+    )
+  }
 }
 
 const ResponsiveContainer = ({ children }) => (
   <div>
     <DesktopContainer>{children}</DesktopContainer>
+    <TabletContainer>{children}</TabletContainer>
     <MobileContainer>{children}</MobileContainer>
   </div>
 )
-
-ResponsiveContainer.propTypes = {
-  children: PropTypes.node,
-}
 
 const Homepage = () => (
   <ResponsiveContainer>
