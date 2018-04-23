12579556261f3d38994143e1b9afab07aa38e584
Tim Ninan <tim.ninan@gmail.com>
2018-04-23 18:07:28 -0500

    added request endpoint for mysong app

--- server/index.js
+++ server/index.js
@@ -11,9 +11,14 @@ app.get('/api', function (req, res) {
     res.send('{"message":"Hello from the custom server!"}');
 });
 
+app.get('/mysong', function (req, res) {
+  res.redirect(302, 'https://tranquil-hollows-12097.herokuapp.com');
+});
+
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
+
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`);
