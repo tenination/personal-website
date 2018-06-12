0019d89e51522fc9e81959d7ea24c029eb309851
Tim Ninan <tninan@C02TH0ZXGTFM.attlocal.net>
2018-06-11 21:31:37 -0500

    removed unused import items

--- client/src/Renovation.js
+++ client/src/Renovation.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <h1 className="App-title">This site is currently being renovated. Please check back later!</h1>
+      <div>
+          <h1>This site is currently being renovated. Please check back later!</h1>
       </div>
     );
   }
